b3afc3fdec1fcfd27c3f4c861c99ddc5f8ce7500
Tristan Sullivan <tssulliv@heplw64.phys.uvic.ca>
2020-11-19 17:02:58 -0800

    Reworded description

--- gen-sim-reco/belle2-gen-sim-reco/parseResults.py
+++ gen-sim-reco/belle2-gen-sim-reco/parseResults.py
@@ -15,34 +15,35 @@ TotalScores = []
 MaxGen = 0
 AvgGen = 0
 MedGen = 0
+MinGen = 1000000
+
 MaxSim = 0
 AvgSim = 0
 MedSim = 0
+MinSim = 1000000
+
 MaxTrigSim = 0
 AvgTrigSim = 0
 MedTrigSim = 0
+MinTrigSim = 1000000
+
 MaxReco = 0
 AvgReco = 0
 MedReco = 0
+MinReco = 1000000
+
 MaxTotal = 0
 AvgTotal = 0
 MedTotal = 0
-MinGen = 1000000
-MinSim = 1000000
-MinTrigSim = 1000000
-MinReco = 1000000
 MinTotal = 1000000
 
+# Each copy generates an output file
 for i in range(0, int(os.environ['NCOPIES'])):
+	filename = "proc_" + str(i + 1) + "/output"
 
-	
-	if i == 0:
-		dirname = "proc_" + str(i + 1)
-	else:
-		dirname = "../proc_" + str(i + 1)
-
-	os.chdir(dirname)
-
+	# The parser reports separately the time taken by event generation, simulation, trigger simulation, 
+	# and reconstruction (and the total). Times are reported from many sub-modules; these keywords 
+	# are used to make the groupings
 	keywords = ["EvtGenInput", "Sum_Simulation", "Sum_TriggerSimulation", "SoftwareTrigger"]
 	keepgoing = False;
 	startintwo = False;
@@ -54,14 +55,18 @@ for i in range(0, int(os.environ['NCOPIES'])):
 	Total = 0
 	temptotal = 0
 
-	with open("output") as out:
+	with open(filename) as out:
 		for line in out:
+
+			# Flag to start parsing, initially false
 			if keepgoing:
 				splitline = line.split("|")
 				#print splitline
+
+				# Lines with Sum_ are the total time taken by the executable, not just
+				# event processing calls
 				if "Sum_" not in line:
 					#print splitline
-					#total = total + float(splitline[3].strip())
 					temptotal = temptotal + float(splitline[3].strip())
 
 				if splitline[0].strip() in keywords:
@@ -81,15 +86,21 @@ for i in range(0, int(os.environ['NCOPIES'])):
 			
 				#print splitline[0].strip() + ": " + splitline[3].strip()
 
+			# Almost there...
 			if startintwo:
 				keepgoing = True
 				startintwo = False
+
+			# This line is two lines before the part we care about, and is more distinctive
+			# than the next line
 			if "Name" in line and "Calls" in line:
 				startintwo = True
 				#print line	
+
 			if "Total" in line: 
 				Total = float(line.split("|")[3].strip())
 
+	# Output is in seconds; convert to events / second
 	Gen = float(os.environ['NEVENTS_THREAD']) / Gen
 	Sim = float(os.environ['NEVENTS_THREAD']) / Sim
 	TrigSim = float(os.environ['NEVENTS_THREAD']) / TrigSim
@@ -163,7 +174,6 @@ else:
 	MedTotal = TotalScores[len(TotalScores) / 2]
 
 
-
 OutputJSON = {}
 OutputJSON['run_info'] = {}
 OutputJSON['report'] = {}
@@ -184,9 +194,9 @@ OutputJSON['report']['wl-stats']['throughput_score']['median'] = MedTotal
 OutputJSON['report']['wl-stats']['throughput_score']['min'] = MinTotal
 OutputJSON['report']['wl-stats']['throughput_score']['max'] = MaxTotal
 OutputJSON['report']['wl-stats']['throughput_score']['count'] = int(os.environ['NCOPIES'])
-OutputJSON['app']['version'] = "v0.15"
+OutputJSON['app']['version'] = "v1.0"
 OutputJSON['app']['description'] = "Belle-2 generation, simulation, and reconstruction of BBbar events based on release 05-01-05"
 
-OutputFile = open("../belle2-gen-sim-reco_summary.json", "w")
+OutputFile = open("belle2-gen-sim-reco_summary.json", "w")
 json.dump(OutputJSON, OutputFile)
 OutputFile.close()
